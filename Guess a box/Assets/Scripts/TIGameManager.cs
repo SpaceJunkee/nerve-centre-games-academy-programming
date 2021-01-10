7777b7ba853c1550e12c373a0ad74cd10d8ba956
Kevin <ksandy@outlook.ie>
2021-01-10 15:31:11 +0000

    Done!

---
+++ Guess a box/Assets/Scripts/TIGameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class TIGameManager : MonoBehaviour
+{
+  
+    int largeGold = 0;
+    int mediumGold = 0;
+    int smallGold = 0;
+    int noGold = 0;
+
+    public static int totalGold = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateGoldValues();
+    }
+
+    public void UpdateGoldValues()
+    {
+        largeGold = Random.Range(601, 1000);
+        mediumGold = Random.Range(301, 600);
+        smallGold = Random.Range(1, 300);
+        noGold = 0;
+    }
+
+    public int GetGoldValue()
+    {
+        int[] goldvalues = { largeGold, mediumGold, smallGold, noGold };
+
+        return goldvalues[Random.Range(0, goldvalues.Length)];
+    }
+
+    public void EndGame()
+    {
+        Debug.Log("YOU LOSE!");
+    }
+}
---
+++ Guess a box/Assets/Scripts/TreasureChestController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreasureChestController : MonoBehaviour
+{
+    TIGameManager ts;
+    Text goldScore;
+    int goldValue;
+
+    //OPTIONAL - IF WE HAVE ENOUGH TIME ON WEEK 3
+    public Animator anim;
+    public Light goldLight;
+
+    private void Start()
+    {
+        goldScore = GameObject.Find("GoldScore").GetComponent<Text>();
+        ts = GameObject.Find("GameManager").GetComponent<TIGameManager>();
+
+        ts.UpdateGoldValues();
+        goldValue = ts.GetGoldValue();
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log(goldValue.ToString());
+        Debug.Log("CLicked " + this.name);
+
+        anim.SetTrigger("OpenChest");
+        
+        if(goldValue > 0)
+        {
+            goldLight.color = new Color32(253, 255, 0, 255);
+        }
+        else
+        {
+            goldLight.color = new Color32(255, 30, 0, 255);
+        }
+
+        if(goldValue > 0)
+        {
+            TIGameManager.totalGold += goldValue;
+            goldScore.text = "GOLD: " + TIGameManager.totalGold.ToString();
+        }
+        else
+        {
+            goldScore.text = " YOU LOSE! ";
+            TIGameManager.totalGold = 0;
+        }
+
+        ts.UpdateGoldValues();
+        goldValue = ts.GetGoldValue();
+    }
+}
